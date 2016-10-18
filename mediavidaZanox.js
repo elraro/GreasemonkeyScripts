2b126be0aba3d6ef1d24cb4e5e2c98c09023d3f1
Alberto <elraro@elraro.eu>
2016-10-18 17:15:42 +0200

    Create mediavidaZanox.js

---
+++ mediavidaZanox.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        zanox tpm
+// @namespace   elraro@elraro.eu
+// @description Block zanox links in MV
+// @version     1
+// @grant       none
+// ==/UserScript==
+var scriptCode = new Array(); // Vamos a guardar el script aqui
+scriptCode.push('function amazon(){}'); // Vamos a reemplazar la función
+var script = document.createElement('script'); // Creamos el objeto script
+script.innerHTML = scriptCode.join('\n');
+scriptCode.length = 0;
+document.getElementsByTagName('head') [0].appendChild(script);
+// fuk u MV
